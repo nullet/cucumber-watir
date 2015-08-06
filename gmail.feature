51a6fd5f69803e16b3310ed6320498074d0c56d6
nullet <tomnullet@gmail.com>
2015-08-06 16:56:51 -0500

    basic challenge complete

---
+++ gmail.feature
@@ -0,0 +1,15 @@
+# See comments in steps file
+
+Feature: As a valid user I should be able to log in to Gmail.
+    
+  Scenario: I log in under my username and the valid password
+    Given I am at gmail
+    And I enter "username" into the username field
+    Then I click on element having id "next"
+    And I wait 2 sec
+    And I enter "password" into the password field
+    Then I click on element having id "signIn"
+    And I wait 10 sec
+    Then I click on the first email
+    And I wait 10 sec
+    Then I close the browser
---
+++ step_definitions/gmail_steps.rb
@@ -0,0 +1,46 @@
+require "watir-webdriver"
+require "rspec/expectations"
+
+Given /^I am at gmail$/ do
+  @browser ||= Watir::Browser.new :firefox
+  @browser.goto "gmail.com"
+end
+
+And /^I enter "([^"]*)" into the username field$/ do |username|
+  @browser.text_field(:id => "Email").set username
+end
+
+Then /^I click on element having id "([^"]*)"$/ do |id|
+  @browser.element(:id => id).click
+end
+
+And /^I wait ([^"]*) sec$/ do |time|
+  sleep time.to_i
+end
+
+And /^I enter "([^"]*)" into the password field$/ do |password|
+  @browser.text_field(:id => "Passwd").set password
+end
+
+Then /^I click on the first email$/ do
+  email = @browser.table(:index => 5)[0][5]
+  email.click
+end
+
+Then /^I close the browser$/ do 
+  @browser.close
+end
+
+=begin
+  This code works, but obviously you'll need to substitute valid
+  credentials for it to do so.
+
+  Not counting some struggles I had with installation, this challenge
+  took me about 45-50 minutes to complete. After figuring out syntax,
+  the bulk of my time was spent figuring out how to select the email 
+  I want - primarily because I had some trouble selecting the appropriate
+  table without any reliable classes or IDs.
+
+  Also, sorry about the utter lack of commit history - just found out
+  at the end that I should submit via Github.
+=end
